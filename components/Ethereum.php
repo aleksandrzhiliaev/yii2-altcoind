f01a604f07f02a0e3f70436f1fb2290ea35469e7
alexanderzhilyaev <sassoftinc@gmail.com>
2017-09-23 17:08:35 +0200

    Ethereum sendTransaction support

--- components/Ethereum.php
+++ components/Ethereum.php
@@ -8,7 +8,8 @@ use yii\httpclient\Client;
 
 class Ethereum extends Component
 {
-    
+    const ETHER_DIGITS = 1000000000000000000;
+
     /**
      * @var RpcClient
      */
@@ -31,20 +32,48 @@ class Ethereum extends Component
         return $data['result'];
     }
 
-    public function send($address, $amount)
+    public function send($fromAddress, $toAddress, $amount)
     {
-        #TODO: sendTransaction support
+        $value = $amount * self::ETHER_DIGITS;
+
+        $gas = 21000;
+        $gasPrice = 21000000000;
+        $tranFee = $gasPrice * $gas;
+        $value -= $tranFee;
+
+        $data = $this->ethereumClient->call('personal_unlockAccount', [$fromAddress, '', 0]);
+        if (isset($data['error'])) {
+            throw new \Exception('personal_unlockAccount error raised: '.$data['error']['message']);
+        }
+
+        $params = [
+            'from' => $fromAddress,
+            'to' => $toAddress,
+            'gas' => '0x'.dechex($gas),
+            'gasPrice' => '0x'.dechex($gasPrice),
+            'value' => '0x'.dechex($value),
+        ];
+
+        $data = $this->ethereumClient->call('eth_sendTransaction', [$params]);
+        if (isset($data['error'])) {
+            throw new \Exception('eth_sendTransaction error raised: '.$data['error']['message']);
+        }
+
+        // return txid
+        return $data['result'];
     }
 
     public function getInfo()
     {
         $info['balance'] = 0;
+        $info['accounts'] = [];
         $data = $this->ethereumClient->call('eth_accounts', []);
         $addresses = $data['result'];
 
         foreach ($addresses as $address) {
             $data = $this->ethereumClient->call('eth_getBalance', [$address, 'latest']);
-            $info['balance'] += floatval($data['result']);
+            $info['accounts'][$address] = floatval(hexdec($data['result'])) / self::ETHER_DIGITS;
+            $info['balance'] += $info['accounts'][$address];
         }
 
         return $info;
